a339bcc02b6cf5677f1b11c24a16214638f1d035
devansh911 <devanshverma911@gmail.com>
2024-08-12 17:54:36 +0530

    Backend work

--- codmav_recommender_system/frontend/recommender_system/src/GraphComponent3.jsx
+++ codmav_recommender_system/frontend/recommender_system/src/GraphComponent3.jsx
@@ -15,23 +15,23 @@ import {
 } from '@chakra-ui/react';
  
 cytoscape.use(coseBilkent);
-const setCookie = (name, value, days) => {
-    const d = new Date();
-    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-};
+// const setCookie = (name, value, days) => {
+//     const d = new Date();
+//     d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+//     const expires = "expires=" + d.toUTCString();
+//     document.cookie = name + "=" + value + ";" + expires + ";path=/";
+// };
 
-const getCookie = (name) => {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-};
+// const getCookie = (name) => {
+//     const nameEQ = name + "=";
+//     const ca = document.cookie.split(';');
+//     for (let i = 0; i < ca.length; i++) {
+//         let c = ca[i];
+//         while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+//         if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+//     }
+//     return null;
+// };
 
  
 const GraphComponent3 = ({ initialSearchQuery }) => {
@@ -46,13 +46,13 @@ const GraphComponent3 = ({ initialSearchQuery }) => {
     const [fuse, setFuse] = useState(null);
     const { isOpen, onOpen, onClose } = useDisclosure();
 
-    useEffect(() => {
-        const hasVisited = getCookie('hasVisited');
-        if (!hasVisited) {
-            onGuideOpen();
-            setCookie('hasVisited', 'true', 365);
-        }
-    }, []);
+    // useEffect(() => {
+    //     const hasVisited = getCookie('hasVisited');
+    //     if (!hasVisited) {
+    //         onGuideOpen();
+    //         setCookie('hasVisited', 'true', 365);
+    //     }
+    // }, []);
 
  
     const fetchNames = async () => {
