b8a79b06124e34db99caf1cbbea26773c9b83262
Digvijay Narayan <digvijayn1993@gmail.com>
2024-09-12 18:52:32 +0530

    removed navbar padding

--- codmav_recommender_system/frontend/recommender_system/src/Navbar.jsx
+++ codmav_recommender_system/frontend/recommender_system/src/Navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
                 <div className="logo-container">
                     <img src="https://research.pes.edu/wp-content/uploads/2023/03/PESU-new-logo.png" alt="PESU Logo" />
                 </div>
-                <div className="nav-links">
                     <a href="http://localhost:3000/centres">Centres</a>
                     <a href="http://localhost:3000/professors">Faculty</a>
                     <a href='http://localhost:3000/rprogram'>PHD program</a>
@@ -17,9 +16,7 @@ const Navbar = () => {
                     <a href='http://localhost:3000/journals'>journals</a>
                     <a href='http://localhost:3000/journals'>Community</a>
                     <a href="http://localhost:3000/research-support">Research Support</a>
-                    <a href="http://localhost:3000/research-support">contact-us</a>
-                    {/* <a href="http://localhost:3000/login">login</a> */}
-                </div>
+                    <a href="http://localhost:3000/Contact">contact-us</a>
             </div>
         </div>
      );
